7bbb079508c72926774b584439ad7822029e252e
Joshua Tanner <biolsurfer85@gmail.com>
2014-10-28 20:45:00 -0700

    remove old comments

--- src/classybrew.js
+++ src/classybrew.js
@@ -1,18 +1,6 @@
 // Author : Joshua Tanner
 // Date : 10-26-2014
 
-// cb = new classyBrew();
-// cb.setSeries([1, 2, 3])
-// cb.setNumClasses(7)
-// cb.setColor('blue')
-// cb.getSeries()
-// cb.getNumClasses()
-// cb.getBreaks()
-// 
-// cb.getClassJenks()
-// cb.getColors()
-
-
 var classyBrew;
 
 classyBrew = function () {
@@ -88,11 +76,6 @@ classyBrew = function () {
 
 	// return array of natural breaks
 	this.classify = function () {
-		// now iterate through the datalist:
-		// determine mat1 and mat2
-		// really not sure how these 2 different arrays are set - the code for
-		// each seems the same!
-		// but the effect are 2 different arrays: mat1 and mat2
 		var mat1 = []
 		for ( var x = 0, xl = this.series.length + 1; x < xl; x++) {
 			var temp = []
@@ -111,9 +94,6 @@ classyBrew = function () {
 			mat2.push(temp2)
 		}
 
-		// absolutely no idea what this does - best I can tell, it sets the 1st
-		// group in the
-		// mat1 and mat2 arrays to 1 and 0 respectively
 		for ( var y = 1, yl = this.numClasses + 1; y < yl; y++) {
 			mat1[0][y] = 1
 			mat2[0][y] = 0
@@ -123,12 +103,6 @@ classyBrew = function () {
 			var v = 0.0
 		}
 
-		// and this part - I'm a little clueless on - but it works
-		// pretty sure it iterates across the entire dataset and compares each
-		// value to
-		// one another to and adjust the indices until you meet the rules:
-		// minimum deviation
-		// within a class and maximum separation between classes
 		for ( var l = 2, ll = this.series.length + 1; l < ll; l++) {
 			var s1 = 0.0
 			var s2 = 0.0
@@ -157,29 +131,22 @@ classyBrew = function () {
 		var k = this.series.length
 		var kclass = []
 
-		// fill the kclass (classification) array with zeros:
 		for (i = 0, il = this.numClasses + 1; i < il; i++) {
 			kclass.push(0)
 		}
 
-		// this is the last number in the array:
 		kclass[this.numClasses] = parseFloat(this.series[this.series.length - 1])
-		// this is the first number - can set to zero, but want to set to lowest
-		// to use for legend:
+
 		kclass[0] = parseFloat(this.series[0])
 		var countNum = this.numClasses
 		while (countNum >= 2) {
 			var id = parseInt((mat1[k][countNum]) - 2)
 			kclass[countNum - 1] = this.series[id]
 			k = parseInt((mat1[k][countNum] - 1))
-			// spits out the rank and value of the break values:
-			// console.log("id="+id,"rank = " + String(mat1[k][countNum]),"val =
-			// " + String(this.series[id]))
-			// count down:
+
 			countNum -= 1
 		}
-		// check to see if the 0 and 1 in the array are the same - if so, set 0
-		// to 0:
+
 		if (kclass[0] == kclass[1]) {
 			kclass[0] = 0
 		}
