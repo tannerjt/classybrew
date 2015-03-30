f089218f42b918db642a2f0305c21c55d973f702
Joshua Tanner <suppor@tannergeo.com>
2015-03-29 19:58:50 -0700

    Add todos

--- src/classybrew.js
+++ src/classybrew.js
@@ -1,18 +1,3 @@
-// Author : Joshua Tanner
-// Date : 10-26-2014
-// todo:
-// 1. Add all color schemes - Done
-// 2. Include additional statistical breaks methods - Done
-// 3. Make compatible with node -Done
-// 4. Add support for geojson - WAIT
-// 5. Add tests 
-// 6. Update documentation
-// 7. Provide better examples
-// 8. Setup Travis-CI
-// 9. Add grunt tasks (linting)
-// 10. Add .gitignore
-// 11. Add an actual todo doofus
-
 (function () {
 
 	var classyBrew = (function () {
---
+++ todo.md
@@ -0,0 +1,13 @@
+## TODO's ##
+
+1. Add all color schemes - Done
+2. Include additional statistical breaks methods - Done
+3. Make compatible with node -Done
+4. Add support for geojson - WAIT
+5. Add tests 
+6. Update documentation
+7. Provide better examples
+8. Setup Travis-CI
+9. Add grunt tasks (linting)
+10. Add .gitignore
+11. Add an actual todo doofus
