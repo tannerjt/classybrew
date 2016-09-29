37803f6045b1506f8f6dd4c85ef250135368ca97
tannerjt <biolsurfer85@gmail.com>
2016-09-29 14:20:54 -0700

    add tests

--- test/test.js
+++ test/test.js
@@ -27,3 +27,16 @@ test('TEST STATISTICAL METHODS', function (t) {
 	t.equal(Array.isArray(breaks), true, 'Standard deviation returns array');
 
 });
+
+test('TEST ZERO VALUES', function (t) {
+	t.plan(1);
+
+	var brew = new classybrew();
+	brew.setSeries([0, 0, 0, 0, 5859, 102141, 198942, 378736]);
+	brew.setNumClasses(5);
+	brew.setColorCode("BuGn");
+	brew.classify('jenks');
+
+	var color1 = brew.getColors()[0];
+	t.equal(brew.getColorInRange(0), color1, '0 value returns first color');
+});
